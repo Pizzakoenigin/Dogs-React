22510e3df5dd71c010dd378920cae346ce089f73
Franziska <Franziska.Roder@gmx.de>
2024-03-28 10:20:38 +0100

    fixed Slider appearance

--- src/MultiRangeSlider.js
+++ src/MultiRangeSlider.js
@@ -3,42 +3,45 @@ import { ContextFilter } from "./Main";
 
 export default function MultiRangeSlider({ }) {
     // create two statuses for min and max range of the filter. these are used in the slider an the numbers input
-    const {filterRangeMin, setfilterRangeMin} = useContext(ContextFilter)
-    const {filterRangeMax, setFilterRangeMax} = useContext(ContextFilter)
+    const { filterRangeMin, setfilterRangeMin } = useContext(ContextFilter)
+    const { filterRangeMax, setFilterRangeMax } = useContext(ContextFilter)
 
     if (filterRangeMin > filterRangeMax || filterRangeMin == 100) {
         setfilterRangeMin(filterRangeMax)
     }
-// with the power of ccs two sliders are created on top of each other (z value)
+    // with the power of ccs two sliders are created on top of each other (z value)
     return (
         <div className="sliderSize">
-            <input
-                type="range"
-                min="10"
-                max="100"
-                className="thumb thumb-3"
-                value={filterRangeMin}
-                onChange={(event) => {
-                    setfilterRangeMin(event.target.value)
-                }}
-            />
+            <div className="slider">
+                <input
+                    type="range"
+                    min="10"
+                    max="100"
+                    className="thumb thumb-3"
+                    value={filterRangeMin}
+                    onChange={(event) => {
+                        setfilterRangeMin(event.target.value)
+                    }}
+                />
 
-            <input
-                type="range"
-                min="10"
-                max="100"
-                className="thumb thumb-4"
-                value={filterRangeMax}
-                onChange={(event) => {
-                    setFilterRangeMax(event.target.value)
-                }}
-            />
+                <input
+                    type="range"
+                    min="10"
+                    max="100"
+                    className="thumb thumb-4"
+                    value={filterRangeMax}
+                    onChange={(event) => {
+                        setFilterRangeMax(event.target.value)
+                    }}
+                />
 
-            <div className="slider" />
-            {/*   multiply by 2 bc the slider is 200px width. but min is a value between 0 and 100. so multiply by two to cover the whole area  */}
-            {/*  here i multiply with 2.1 because otherwise it looks weird*/}
-            <div className="coverAreaSlider" style={{ left: filterRangeMin * 2 + "px", width: (filterRangeMax - filterRangeMin) * 2.1 + "px" }}>
+                <div className="areaSlider" />
+                {/*   multiply by 2 bc the slider is 200px width. but min is a value between 0 and 100. so multiply by two to cover the whole area  */}
+                {/*  here i multiply with 1.8 because otherwise it looks weird*/}
+                <div className="coverAreaSlider" style={{ left: (filterRangeMin * 1.8) + "px", width: (filterRangeMax - filterRangeMin) * 2 + "px" }}>
+                </div>
             </div>
+
 
             <div className="numberInputSlider">
                 {/* regular number input */}
